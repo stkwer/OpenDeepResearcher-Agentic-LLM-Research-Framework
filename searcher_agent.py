eab6fadf65509807a5a17b447de7dc8112a9ebf1
Anjali Mishra <anjalimishraaa2004@gmail.com>
2026-01-22 21:56:02 -0800

    Add files via upload

---
+++ searcher_agent.py
@@ -0,0 +1,9 @@
+class SearcherAgent:
+    def __init__(self):
+        pass
+
+    def run(self, questions):
+        return {
+            q: "Dummy research data for testing"
+            for q in questions
+        }
