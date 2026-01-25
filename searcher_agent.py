86f592bee1a89427987cd9f43b06ff33cc5b351b
sri krishna priya <srikrishnapriyas1@gmail.com>
2026-01-25 11:49:02 +0530

    Remove default agent files from my branch

--- searcher_agent.py
+++
@@ -1,9 +0,0 @@
-class SearcherAgent:
-    def __init__(self):
-        pass
-
-    def run(self, questions):
-        return {
-            q: "Dummy research data for testing"
-            for q in questions
-        }
